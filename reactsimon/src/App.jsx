bd24bcfaf857a416da4cd3a6948d50378a5b301b
simon.horrabin <simonhorrabin@gmail.com>
2023-07-13 12:05:24 -0400

    initial commit

---
+++ reactsimon/src/App.jsx
@@ -0,0 +1,3 @@
+App
+
+export default App;
---
+++ reactsimon/src/index.js
@@ -0,0 +1,10 @@
+import ReactDom from 'react-dom';
+import App from './App'
+import './index.css';
+
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.render(<App/>, document.querySelector("#root"));
+
+
+
