b609b32724aba9a5a0a8c944749935b9f966d0d8
simon.horrabin <simonhorrabin@gmail.com>
2023-07-15 02:56:59 -0400

    nav bar

--- reactsimon/src/components/Nav/Nav.jsx
+++ reactsimon/src/components/Nav/Nav.jsx
@@ -4,13 +4,15 @@ import { AiOutlineHome } from "react-icons/ai"
 import {RiContactsBook2Fill} from "react-icons/ri"
 import {FaHome} from "react-icons/fa"
 import {MdCatchingPokemon} from "react-icons/md"
+import {MdComputer} from "react-icons/md"
+
 const Nav = () => {
     return (
         <nav>
             <a href="#home"><FaHome/></a>
             <a href="#about"><MdCatchingPokemon/></a>
             <a href="#contact"><RiContactsBook2Fill/></a>
-            <a href="#services"><AiOutlineHome/></a>
+            <a href="#services"><MdComputer/></a>
         </nav>
     )
 }
