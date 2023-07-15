298c671164dd0cfc409193e56f754e1bce11bf9f
simon.horrabin <simonhorrabin@gmail.com>
2023-07-15 02:28:06 -0400

    icons

--- reactsimon/src/components/Nav/Nav.jsx
+++ reactsimon/src/components/Nav/Nav.jsx
@@ -1,10 +1,18 @@
 import React from "react"
 import './nav.css'
-
+import { AiOutlineHome } from "react-icons/ai"
+import {RiContactsBook2Fill} from "react-icons/ri"
+import {FaHome} from "react-icons/fa"
+import {MdCatchingPokemon} from "react-icons/md"
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <nav>
+            <a href="#home"><FaHome/></a>
+            <a href="#about"><MdCatchingPokemon/></a>
+            <a href="#contact"><RiContactsBook2Fill/></a>
+            <a href="#services"><AiOutlineHome/></a>
+        </nav>
     )
 }
 
-export default Nav
+export default Nav 
